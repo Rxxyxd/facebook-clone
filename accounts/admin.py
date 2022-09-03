5ae4bfd44bd6a649813908cc4472e26d6e343d29
Joshua Abbey <joshuaabbey2022@gmail.com>
2022-09-03 03:24:45 +0100

    custom user model / migrated

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display =[
+        "email",
+        "username",
+        "gender",
+        "dob",
+        "is_staff",
+    ]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("gender", "dob",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("gender", "dob",)}),)
+    
+admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import datetime
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    dob = models.DateField(max_length=8, default=datetime.date.today, null=True, blank=True)
+    gender_choice = [('M', 'Male'),('F', 'Female')]
+    gender = models.CharField(choices=gender_choice, max_length=128, null=True)
